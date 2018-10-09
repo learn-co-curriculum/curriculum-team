a6a7fa3a0141905109a9a7bf7136d1c6477ac365
Steven G. Harms <steven.harms@flatironschool.com>
2018-10-09 12:15:01 -0400

    Update initializer code

--- curriculum-initialize.sh
+++ curriculum-initialize.sh
@@ -46,7 +46,7 @@ EOT
 cat > LICENSE.md <<EOT
 # Learn.co Educational Content License
 
-Copyright (c) 2015 Flatiron School, Inc
+Copyright (c) 2018 Flatiron School, Inc
 
 The Flatiron School, Inc. owns this Educational Content. However, the Flatiron
 School supports the development and availability of educational materials in
@@ -56,7 +56,7 @@ upon and share such Educational Content subject to the terms of the Educational
 Content License set forth [here](http://learn.co/content-license)
 (http://learn.co/content-license). You must read carefully the terms and
 conditions contained in the Educational Content License as such terms govern
-access to and use of the Educational Content.  
+access to and use of the Educational Content.
 
 Flatiron School is willing to allow you access to and use of the Educational
 Content only on the condition that you accept all of the terms and conditions
