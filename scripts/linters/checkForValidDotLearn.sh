d3a747a55e058dc43030e0715fd738415b26df68
Maxwell Benton <maxwellbenton@gmail.com>
2019-05-21 12:56:38 -0400

    adds check for deprecation

--- scripts/linters/checkForValidDotLearn.sh
+++ scripts/linters/checkForValidDotLearn.sh
@@ -2,11 +2,18 @@ if [ -a '.learn' ]
 
 then
   if ! grep -q 'tags:' '.learn'; then
-    echo '- Missing tags'
+    echo '- Missing tags - Recommended'
   fi
   if ! grep -q 'languages:' '.learn'; then
-    echo '- Missing languages'
+    echo '- Missing languages - Recommended'
+  fi
+  if grep -q 'after_ide_submission' '.learn'; then
+    echo 'WARNING: after_ide_submission found - Deprecated - Must remove'
+  fi
+  if grep -q 'github: false' '.learn'; then
+    echo 'WARNING: github: false found - Deprecated - Must remove'
   fi
 else
-  echo '- Missing .learn file'
+  echo 'WARNING: Missing .learn file - Required'
 fi
+
