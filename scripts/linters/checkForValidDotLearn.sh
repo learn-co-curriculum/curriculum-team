e90ca96e6271a1a9aefaab80a2ef040f1d3ce26e
Maxwell Benton <maxwellbenton@gmail.com>
2019-05-22 17:33:47 -0400

    echoes repo name, checks for language singular

--- scripts/linters/checkForValidDotLearn.sh
+++ scripts/linters/checkForValidDotLearn.sh
@@ -1,20 +1,25 @@
 if [ -a '.learn' ]; then
-  if [ ! -s '.learn' ]; then
-    echo 'WARNING: .learn Must not be blank'
-  fi
-  if ! grep -q 'tags:' '.learn'; then
-    echo '- Missing tags - Recommended'
-  fi
-  if ! grep -q 'languages:' '.learn'; then
-    echo '- Missing languages - Recommended'
-  fi
-  if grep -q 'after_ide_submission' '.learn'; then
-    echo 'WARNING: after_ide_submission found - Deprecated - Must remove'
-  fi
-  if grep -q 'github: false' '.learn'; then
-    echo 'WARNING: github: false found - Deprecated - Must remove'
-  fi
-else
-  echo 'WARNING: Missing .learn file - Required'
-fi
 
+  if [ ! -s '.learn' ]; then
+    echo "${PWD##*/} - WARNING: .learn Must not be blank"
+  fi
+  
+  if ! grep -q 'tags:' '.learn'; then
+    echo "${PWD##*/} - Missing tags - Recommended"
+  fi
+
+  if ! { grep -q 'languages:' '.learn' || grep -q 'language:' '.learn'; }; then
+    echo "${PWD##*/} - Missing language - Recommended"
+  fi
+  
+  if grep -q 'after_ide_submission' '.learn'; then
+    echo "${PWD##*/} - WARNING: after_ide_submission found - Deprecated - Must remove"
+  fi
+
+  if grep -q 'github: false' '.learn'; then
+    echo "${PWD##*/} - WARNING: github: false found - Deprecated - Must remove"
+  fi
+
+else
+  echo "${PWD##*/} - WARNING: Missing .learn file - Required"
+fi
