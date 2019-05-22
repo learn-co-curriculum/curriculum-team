b75a1cd016525a2a3a29ea880de019257d59b30b
Maxwell Benton <maxwellbenton@gmail.com>
2019-05-22 17:31:40 -0400

    checkout --quiet and echoing repo name

--- scripts/linters/checkForLabSolutionBranch.sh
+++ scripts/linters/checkForLabSolutionBranch.sh
@@ -1,13 +1,12 @@
-if [ -d 'spec' ] || [ -d 'test' ]
-then
-  git co solution > /dev/null
+if [ -d 'spec' ] || [ -d 'test' ]; then
+  git co solution --quiet
   branch_name=$(git symbolic-ref -q HEAD)
   branch_name=${branch_name##refs/heads/}
   branch_name=${branch_name:-HEAD}
   
   if [ $branch_name != 'solution' ]
   then
-    echo '- No solution branch found'
+    echo "${PWD##*/} - No solution branch found"
   fi
-  git co master > /dev/null
+  git co master --quiet
 fi
