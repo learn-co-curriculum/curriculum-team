a04629ff68314aed2d0bb5c75e333088cb442452
Rob Cobb <rwcobbjr@gmail.com>
2019-12-02 13:43:10 -0500

    run summary without args

--- tools/summary.sh
+++ tools/summary.sh
@@ -3,7 +3,9 @@
 # Important note: Make sure you're using sgharms' fork of github/hub
 # so that you have the -r flag. Integration to github/hub is pending.
 
-# Usage: ./issues_summary.sh lesson_lists issues_output
+# Usage: ./summary.sh
+# or
+# ./summary.sh path/to/lesson_lists path/to/issues_output
 
 github_link_from_ssh_to_https() {
   temp="$(echo $1 | sed 's+git@github.com:+https://github.com/+g')"
@@ -14,14 +16,22 @@ process_sourcefile () {
   cat "$1" | hub issue -r STDIN --include-pulls -f '* [%ct][%au]: %t %U%n' >> "$2"
 }
 
-DEST_DIR_PREFIX="$2/$(date "+%Y-%m-%d")"
 
 if [ -z "$1" ]
 then
-  SEARCH_DIR="${PWD}"
+  SEARCH_DIR="lesson_lists"
 else
   SEARCH_DIR="$1"
 fi
+
+if [ -z "$2" ]
+then
+  DEST_DIR="issues_output"
+else
+  DEST_DIR="$2"
+fi
+
+DEST_DIR_PREFIX="$DEST_DIR/$(date "+%Y-%m-%d")"
 
 mkdir "$DEST_DIR_PREFIX"
 
