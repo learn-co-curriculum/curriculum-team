c56d7419db12c972f1b68564fcb21419faaacf41
github username <github email address>
2020-01-12 20:11:47 -0500

    Updates

---
+++ precache-manifest.32e1db7f1a734ec55c50a78c0056b6af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349835ad95a9695b2fe7960243d05b89",
+    "url": "/curriculum-team/index.html"
+  },
+  {
+    "revision": "d12a894faf247c7b5826",
+    "url": "/curriculum-team/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "749ae69a070a8bd694d5",
+    "url": "/curriculum-team/static/css/main.21ebbb27.chunk.css"
+  },
+  {
+    "revision": "d12a894faf247c7b5826",
+    "url": "/curriculum-team/static/js/2.2fada5fa.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/curriculum-team/static/js/2.2fada5fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "749ae69a070a8bd694d5",
+    "url": "/curriculum-team/static/js/main.e76d4ef6.chunk.js"
+  },
+  {
+    "revision": "ea18d2ac5b0deeeb1dc7",
+    "url": "/curriculum-team/static/js/runtime-main.dd085345.js"
+  }
+]);
