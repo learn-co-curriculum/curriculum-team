e12ad0e47246f9f7fc6f6b96b1691b3cf434b22b
github username <github email address>
2020-01-12 20:12:53 -0500

    Updates

---
+++ precache-manifest.f913230b01fe3c91dea8264f06276425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836350a2fc6a396a4988e465354f11d1",
+    "url": "/curriculum-team/index.html/index.html"
+  },
+  {
+    "revision": "d12a894faf247c7b5826",
+    "url": "/curriculum-team/index.html/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "70110d4f740cec0ce767",
+    "url": "/curriculum-team/index.html/static/css/main.21ebbb27.chunk.css"
+  },
+  {
+    "revision": "d12a894faf247c7b5826",
+    "url": "/curriculum-team/index.html/static/js/2.2fada5fa.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/curriculum-team/index.html/static/js/2.2fada5fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70110d4f740cec0ce767",
+    "url": "/curriculum-team/index.html/static/js/main.0e4b28b1.chunk.js"
+  },
+  {
+    "revision": "f89108330f8cf08cec60",
+    "url": "/curriculum-team/index.html/static/js/runtime-main.48213438.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/curriculum-team/precache-manifest.32e1db7f1a734ec55c50a78c0056b6af.js"
+  "/curriculum-team/index.html/precache-manifest.f913230b01fe3c91dea8264f06276425.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curriculum-team/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/curriculum-team/index.html/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
